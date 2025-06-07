70e5dc4624e96dbe676023611f85ca041dca8c70
khushi2830 <khumarikhushi03@gmail.com>
2025-06-07 15:24:41 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\PublicController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(PublicController::class)->group(function(){
+    Route::get("/", "home")->name("public.home");
+});
