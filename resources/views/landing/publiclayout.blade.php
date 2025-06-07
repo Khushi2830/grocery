7621e213ddf9cc10639825fadfa1776d7b5dbaec
aastha058 <aasthapriya185@gmail.com>
2025-06-07 15:52:00 +0530

    Enhance footer layout with additional sections for Quick Links, Help, and Newsletter

--- resources/views/landing/publiclayout.blade.php
+++ resources/views/landing/publiclayout.blade.php
@@ -31,36 +31,77 @@
     </div>
   </div>
 </nav>
-<nav class="navbar navbar-expand-lg bg-body-success">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="#">Hidden brand</a>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
 
 
   @section('content')
 
   @show
+  <footer class="text-white pt-5 pb-4" style="background-color: #d10075;">
+  <div class="container text-md-left">
+    <div class="row text-md-left">
+
+      <!-- Logo and Tagline -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold">Creamery</h5>
+        <p>Your destination for delicious sweets and snacks. Taste happiness in every bite!</p>
+      </div>
+
+      <!-- Quick Links -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold">Quick Links</h5>
+        <p><a href="#" class="text-white" style="text-decoration: none;">About Us</a></p>
+        <p><a href="#" class="text-white" style="text-decoration: none;">Blog</a></p>
+        <p><a href="#" class="text-white" style="text-decoration: none;">Contact</a></p>
+        <p><a href="#" class="text-white" style="text-decoration: none;">Menu</a></p>
+      </div>
+
+      <!-- Help -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold">Help</h5>
+        <p><a href="#" class="text-white" style="text-decoration: none;">Payment Options</a></p>
+        <p><a href="#" class="text-white" style="text-decoration: none;">Shipping Info</a></p>
+        <p><a href="#" class="text-white" style="text-decoration: none;">FAQ</a></p>
+      </div>
+
+      <!-- Newsletter -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold">Newsletter</h5>
+        <form>
+          <div class="input-group">
+            <input type="email" class="form-control" placeholder="Your email" required>
+            <button class="btn btn-light text-danger fw-bold">Subscribe</button>
+          </div>
+        </form>
+        <div class="mt-3">
+          <a href="#"><i class="fab fa-facebook text-white me-3"></i></a>
+          <a href="#"><i class="fab fa-instagram text-white me-3"></i></a>
+          <a href="#"><i class="fab fa-whatsapp text-white me-3"></i></a>
+        </div>
+      </div>
+
+    </div>
+
+    <hr class="mb-4">
+
+    <!-- Copyright -->
+    <div class="row align-items-center">
+      <div class="col-md-7 col-lg-8">
+        <p>© 2025 Creamery. All rights reserved. | Made with ❤️ for sweet lovers.</p>
+      </div>
+      <div class="col-md-5 col-lg-4">
+        <div class="text-center text-md-right">
+          <img src="assets/visa.png" width="40" class="me-2">
+          <img src="assets/mastercard.png" width="40" class="me-2">
+          <img src="assets/paytm.png" width="40" class="me-2">
+          <img src="assets/razorpay.png" width="40">
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+  
 
 
 
